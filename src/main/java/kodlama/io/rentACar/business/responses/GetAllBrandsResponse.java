ac503f296b736b2d3f00896eb99996695ed1bbd6
Tugce Dalay <tgcdalay@gmail.com>
2024-04-03 16:10:33 +0300

    responded

---
+++ src/main/java/kodlama/io/rentACar/business/responses/GetAllBrandsResponse.java
@@ -0,0 +1,22 @@
+package kodlama.io.rentACar.business.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetAllBrandsResponse {
+   
+	private int id;
+	private String name;
+//	public void setName(String name2) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//	public void setId(int id2) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+}
---
+++ src/main/java/kodlama/io/rentACar/entities/concretes/Brand.java
@@ -0,0 +1,34 @@
+package kodlama.io.rentACar.entities.concretes; 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name="brands")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Brand {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //veriyi otomatik üretsin
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+
+	
+	
+
+}
